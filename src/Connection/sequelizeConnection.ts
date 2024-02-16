e965b2bee548b539133e8dd258f7f8d89b7723b5
prakhart <prakhart@incubxperts.com>
2024-02-16 16:50:43 +0530

    host change

--- src/Connection/sequelizeConnection.ts
+++ src/Connection/sequelizeConnection.ts
@@ -1,7 +1,7 @@
 import { Sequelize,Model,DataType, DataTypes } from 'sequelize';
 
-const sequelize = new Sequelize('testdb', 'root', 'root', {
-    host: 'localhost',
+const sequelize = new Sequelize('testdb', 'root', 'rootmaster', {
+    host: 'db-mssql-rds.c36oimkgarne.ap-south-1.rds.amazonaws.com',
     dialect:'mssql',
     omitNull:true,
     define:{
