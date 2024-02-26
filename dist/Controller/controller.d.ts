9abd99f08ed79cf90325a161036f10e47321f56f
prakhart <prakhart@incubxperts.com>
2024-02-26 12:29:02 +0530

    dist

--- dist/Controller/controller.d.ts
+++ dist/Controller/controller.d.ts
@@ -1,7 +1,7 @@
 declare function getTopicDataController(): Promise<any>;
 declare function getTopicExistsController(data: any): Promise<any>;
-declare function getTopicNameExistsController(topic: any, id: any): Promise<any>;
-declare function editTopicNameController(topic: any, id: any): Promise<any>;
+declare function getTopicNameExistsController(topic: any, id: number): Promise<any>;
+declare function editTopicNameController(topic: any, id: number): Promise<any>;
 declare function saveTopicDataController(data: any): Promise<any>;
 declare const _default: {
     getTopicDataController: typeof getTopicDataController;
